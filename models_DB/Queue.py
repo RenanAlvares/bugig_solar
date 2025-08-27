31adb0d9e4ce1f49acd8824604992032e3503ff5
Renan <renan.s.alvares@gmail.com>
2025-08-26 22:02:23 -0300

    criacao de alguns bancos e atualizacao nos forms

---
+++ models_DB/Queue.py
@@ -0,0 +1,10 @@
+from Main import db
+
+class Queue(db.Model):
+
+    __tablename__ = 'Fila'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_user_fila = db.Column(db.Integer, db.ForeignKey('Usuarios.id'), nullable=False)
+    quantidade_solicitada = db.Column(db.Integer, nullable=False)
+    distribuidora = db.Column(db.String(100), nullable=False)
