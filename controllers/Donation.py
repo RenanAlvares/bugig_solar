66b265328aec7c6ca91d210ec38694d697edf0ca
Renan <renan.s.alvares@gmail.com>
2025-08-31 08:41:30 -0300

    criação de algumas rotas e forms

---
+++ controllers/Donation.py
@@ -0,0 +1,21 @@
+from Main import app
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
+from forms.FormDonation import FormDonation
+from models_DB.Companies import DistribuidoraModel
+
+
+@app.route('/donation', methods=['POST', 'GET'])
+def donation():
+    
+    form = FormDonation()
+    form.distribuidora.choices = [(d.id_distribuidora, d.nome_distribuidora) for d in DistribuidoraModel.query.all()]
+    # seleciona todas as distribuidoras cadastradas no banco
+
+
+    if form.validate_on_submit():
+        qtd = form.qtd_doada.data
+        distribuidora_id = form.distribuidora.data
+
+        print(f'Sua doação de {qtd} unidades foi realizada com sucesso!')
+        return url_for('bugig') # após doar retorna para a tela de menu principal
+    return render_template('donation.html', form=form)
