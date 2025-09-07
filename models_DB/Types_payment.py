e52b54872358b00452ec221ce1d6cb5d1b698053
Renan <renan.s.alvares@gmail.com>
2025-09-07 09:03:03 -0300

    incluindo todos os tipos do banco em um unico arquivo

--- models_DB/Types_payment.py
+++
@@ -1,8 +0,0 @@
-from Main import db
-
-class TipoPagamento(db.Model):
-
-    __tablename__ = 'Tipo_pagamento'
-
-    id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(100), nullable=False)
--- models_DB/Types_user.py
+++
@@ -1,8 +0,0 @@
-from Main import db
-
-class TipoModel(db.Model):
-
-    __tablename__ = 'Tipos_usuarios'
-
-    id_tipo = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome_tipo = db.Column(db.String(20), nullable=False)
