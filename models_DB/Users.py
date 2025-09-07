35c175f7c7c818392f70c86a4176fcbc02eb4348
Renan <renan.s.alvares@gmail.com>
2025-09-07 09:27:48 -0300

    criacao de banco novo e normalizacao

--- models_DB/Users.py
+++ models_DB/Users.py
@@ -18,6 +18,9 @@ class UsersDb(db.Model):
     razao_social = db.Column(db.String(100), nullable = True)
     id_distribuidora = db.Column(db.Integer, db.ForeignKey('Distribuidoras.id_distribuidora'), nullable = False)
 
+    id_tipo = db.relationship('TipoModel', backref='usuarios', lazy=True)
+    distribuidora = db.relationship('Companies', backref='usuarios', lazy=True)
+
 
     def __repr__(self):
         return f'<Usuario {self.nome}>'
