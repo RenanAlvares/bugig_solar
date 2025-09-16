29b6d99c86c8ae688082f70c3011cb3e56e86a7a
Renan <renan.s.alvares@gmail.com>
2025-09-16 19:49:28 -0300

    ajuste nas rotas

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from controllers import login
-from controllers.login import auth_bp
+from flask import Blueprint
 
 app = Flask(__name__)
 
@@ -10,6 +9,7 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 
+auth_bp = Blueprint('auth', __name__)
 app.register_blueprint(auth_bp, url_prefix='/bugig') 
 
 # fazer um teste se tirar o .login funciona para importar tudo
