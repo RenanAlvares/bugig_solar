bb153b1940b760a00ac39dbe3c486d98dd757d95
Renan <renan.s.alvares@gmail.com>
2025-10-02 21:35:23 -0300

    iniciando a função de solicitar créditos.

--- forms/form_queue.py
+++ forms/form_queue.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, SelectField, validators
 
 class FormQueue(FlaskForm):
-    qtd_solicitada = IntegerField('Digite a quantidade solicitada', [validators.InputRequired()])
+    qtd_solicitada = IntegerField('Digite a quantidade a ser solicitada:', [validators.InputRequired()])
 
     # coerce=int converte o valor selecionado para inteiro, pois é inserido um id na tabela de fila
     distribuidora = SelectField('Selecione sua distribuidora', coerce=int, validators=[validators.InputRequired()])
